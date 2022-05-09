090968e64b695d8dab23ef336903d6ac5c93a92a
Clarissa Polidori <73442844+clarisa1997@users.noreply.github.com>
2022-05-09 18:57:36 +0200

    classe del classificatore da provare

--- app/src/main/java/it/unipi/dii/digitalwellbeing_app/Classifier.java
+++ app/src/main/java/it/unipi/dii/digitalwellbeing_app/Classifier.java
@@ -2,46 +2,47 @@ package it.unipi.dii.digitalwellbeing_app;
 
 import android.content.Context;
 
-//import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.DataType;
 
-//import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
-//import it.unipi.dii.digitalwellbeing.ml.Model;
-
 import java.io. * ;
 import java.util.Scanner;
 
+import it.unipi.dii.digitalwellbeing_app.ml.PickupClassifier;
+
 public class Classifier {
 
-     //private Model model;
+     private PickupClassifier model;
      private Context context;
 
     public Classifier(Context ctx) throws IOException {
 
-       /* this.context = ctx;
-        this.model = Model.newInstance(ctx);*/
+       this.context = ctx;
+       this.model = PickupClassifier.newInstance(ctx);
 
 
     }
 
-    /*public void classify(ByteBuffer byteBuffer){
+    public void classify(ByteBuffer byteBuffer){
 
             // Creates inputs for reference.
             TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 4}, DataType.FLOAT32);
             inputFeature0.loadBuffer(byteBuffer);
 
             // Runs model inference and gets result.
-            Model.Outputs outputs = model.process(inputFeature0);
+            PickupClassifier.Outputs outputs = model.process(inputFeature0);
             TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
 
             // Releases model resources if no longer used.
             model.close();
 
 
-    }*/
+    }
 
 
 }
