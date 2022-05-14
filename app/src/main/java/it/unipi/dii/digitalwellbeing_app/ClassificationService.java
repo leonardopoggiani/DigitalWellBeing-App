a95039e4f5f091a80e5406c21ec1e93546c95288
Clarissa Polidori <73442844+clarisa1997@users.noreply.github.com>
2022-05-14 16:30:38 +0200

    iniziato serviceClassifier

--- app/src/main/java/it/unipi/dii/digitalwellbeing_app/ClassificationService.java
+++ app/src/main/java/it/unipi/dii/digitalwellbeing_app/ClassificationService.java
@@ -1,15 +1,28 @@
 package it.unipi.dii.digitalwellbeing_app;
 
+import android.app.IntentService;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
-public class ClassificationService {
+public class ClassificationService extends IntentService {
 
     private final IBinder binder = new LocalBinder();
     private ServiceCallbacks serviceCallbacks;
+    private String TAG = "ClassificationService";
+    private ActivityClassifier classifier;
+    Intent intentResult;
+    boolean status;
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     */
+    public ClassificationService() {
+        super("ClassificationService");
+    }
 
     //Class used for the client Binder
     public class LocalBinder extends Binder {
@@ -18,9 +31,25 @@ public class ClassificationService {
         }
     }
 
+    @Override
+    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+        Log.d(TAG, "Started");
+
+        classifier = new ActivityClassifier(this);
+        intentResult = new Intent(this, MainActivity.class);
+        intentResult.setAction("Classification_Result");
+        status = false;
+        return super.onStartCommand(intent, flags, startId);
+    }
+
     @Nullable
     public IBinder onBind(Intent intent) {
         return binder;
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
     }
 
     public void setCallbacks(ServiceCallbacks callbacks) {
