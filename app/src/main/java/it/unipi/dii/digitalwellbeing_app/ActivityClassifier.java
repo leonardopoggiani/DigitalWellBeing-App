a223c27d3bc2a6f1f712e5c5e07c981f6c184a87
Gio-under <73661783+Gio-under@users.noreply.github.com>
2022-05-28 19:55:48 +0200

    final commit
    final commit

--- app/src/main/java/it/unipi/dii/digitalwellbeing_app/ActivityClassifier.java
+++ app/src/main/java/it/unipi/dii/digitalwellbeing_app/ActivityClassifier.java
@@ -27,29 +27,23 @@ public class ActivityClassifier {
         try {
 
             PickupClassifier model = PickupClassifier.newInstance(ctx.getApplicationContext());
-
             int[] shape = new int[]{1, 18};
             TensorBuffer tensorBuffer = TensorBuffer.createFixedSize(shape, DataType.FLOAT32);
-
             for (int i = 0; i < toClassify.length; i++) {
                 data[i] = toClassify[i];
             }
-
             tensorBuffer.loadArray(data);
-
             inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 18, 1}, DataType.FLOAT32);
             ByteBuffer byteBuffer = tensorBuffer.getBuffer();
             inputFeature0.loadBuffer(byteBuffer);
-
             // Runs model inference and gets result.
             PickupClassifier.Outputs outputs = model.process(inputFeature0);
             TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
             data = outputFeature0.getFloatArray();
-
             if(!pocket) {
                 pocket = (data[0] > 0.85) && (SensorHandler.already_recognized);
             }
-
+            
             if(pocket && !SensorHandler.already_recognized) {
                 pickup = true;
                 pocket = false;
